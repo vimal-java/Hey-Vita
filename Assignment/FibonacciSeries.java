65e886d57260a50dd228e011cabe99e254867a8d
vimal-java <64842243+vimal-java@users.noreply.github.com>
2020-05-05 20:55:37 +0530

    Add files via upload

---
+++ Assignment/FibonacciSeries.java
@@ -0,0 +1,63 @@
+package basics.assignments;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+		
+		int range = 8;
+		int firstNum = 0;
+		int secondNum = 1;
+		int tempVariable;
+		
+		for (int i = 1; i <= range; i++) {
+			
+			System.out.println(firstNum);
+			tempVariable = firstNum;
+			firstNum = secondNum;
+			secondNum = firstNum + tempVariable;
+			
+		}
+		
+
+	}
+
+}
+
+/*
+ * Goal: To find Fibonacci Series for a given range
+ * 
+ * input(range): 8
+ * output: 0, 1, 1, 2, 3, 5, 8, 13
+ * 
+ * Shortcuts:
+ * 1) Print : type: syso, followed by: ctrl + space + enter
+ * 2) To create a 'for' loop: type 'for', followed by ctrl + space + down arrow + enter
+ *   
+ * What are my learnings from this code?
+ * 1)
+ * 2)
+ * 3) 
+ * 
+ */
+
+//initialize 3 int variables (Tip: range & firstNum, secNum in the series)
+
+
+
+		// Iterate from 1 to the range
+
+		
+
+			// Print first number 
+
+			
+
+			// add first and second number
+
+			
+
+			// Assign second nuumber to the first number
+
+			
+
+			// Assign sum to the second number
